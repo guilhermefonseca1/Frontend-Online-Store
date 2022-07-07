731a66c6a96049ceecb666d68cb037f909436dc0
Renato Mendes <mendeslrenato@gmail.com>
2022-07-07 16:20:19 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  // Implemente aqui https://api.mercadolibre.com/sites/MLB/categories
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId = '', query = '') {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  return response.json();
 }
+
+// getProductsFromCategoryAndQuery('computador', '');
