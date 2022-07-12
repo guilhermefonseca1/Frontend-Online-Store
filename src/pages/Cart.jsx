a1ef2a58ea4bd01615ca48c73760977cc6c964dd
marcela <arqmrcl@gmail.com>
2022-07-12 20:07:25 -0300

    requisito 12

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -61,6 +61,7 @@ export default class Cart extends Component {
               className="buttonFinalizar"
               type="button"
               onClick={ this.goToFinish }
+              data-testid="checkout-products"
             >
               Finalizar Compra
               <Check
